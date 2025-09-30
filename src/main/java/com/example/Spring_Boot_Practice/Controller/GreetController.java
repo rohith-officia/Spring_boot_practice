d3193936a93885c2a523da26794cf24461141f87
rohith <rohith.officia@gmail.com>
2025-09-30 19:21:24 +0530

    PathVariable

--- src/main/java/com/example/Spring_Boot_Practice/Controller/GreetController.java
+++ src/main/java/com/example/Spring_Boot_Practice/Controller/GreetController.java
@@ -1,20 +1,22 @@
 package com.example.Spring_Boot_Practice.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetController {
 
     @GetMapping("/greet")
     public String greet(){
-        return "Hi!";
+        return "Hi !";
     }
 
     @GetMapping("/greet2")
     public String greet2(@RequestParam String name){
-        return "Hi" + name + "!";
+        return "Hi " + name + "!";
+    }
+
+    @GetMapping("greet3/{name}")
+    public String greet3(@PathVariable String name){
+        return "Hi " + name + "!";
     }
 }
