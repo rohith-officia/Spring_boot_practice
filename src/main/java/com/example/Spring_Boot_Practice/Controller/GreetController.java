3457420ca6f48fd5f5b3f9e18745dc48a8d2cf82
rohith <rohith.officia@gmail.com>
2025-09-30 19:29:21 +0530

    add http path

--- src/main/java/com/example/Spring_Boot_Practice/Controller/GreetController.java
+++ src/main/java/com/example/Spring_Boot_Practice/Controller/GreetController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class GreetController {
 
-    @GetMapping("/greet")
+    @GetMapping("/greet") //http://localhost:8080/greet
     public String greet(){
         return "Hi !";
     }
 
-    @GetMapping("/greet2")
+    @GetMapping("/greet2") //http://localhost:8080/greet2?name=zara
     public String greet2(@RequestParam String name){
         return "Hi " + name + "!";
     }
 
-    @GetMapping("greet3/{name}")
+    @GetMapping("greet3/{name}") //http://localhost:8080/greet3/zara
     public String greet3(@PathVariable String name){
         return "Hi " + name + "!";
     }
