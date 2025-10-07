f5433f22bdd4f4ad7be59bcfca51ccd7506369eb
rohith <rohith.officia@gmail.com>
2025-10-07 19:32:43 +0530

    add data base (PgAdmin)

--- src/main/java/com/example/Spring_Boot_Practice/Repository/UserRepository.java
+++ src/main/java/com/example/Spring_Boot_Practice/Repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.Spring_Boot_Practice.Repository;
 import com.example.Spring_Boot_Practice.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity , Long> {
+public interface UserRepository extends JpaRepository<UserEntity , Long > {//"JPA"
 
 }
